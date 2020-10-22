12d76df204b9fbb32d617c638b67d7cdc851f771
s-pc <s-pc@localhost>
2020-10-22 13:18:51 +0530

    P10 committed

---
+++ 10SpringSecurity_DisplayContentBasedOnUserRoles/src/main/java/com/spring_security/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package com.spring_security.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {MySpringAppConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+}
