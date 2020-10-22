1d1dc51c76af86ba63a60e6df802b4d127e29786
s-pc <s-pc@localhost>
2020-10-22 13:11:56 +0530

    P1 committed

---
+++ 01SpringSecurity_BasicSecurity/src/main/java/com/spring_security/config/MySecurityConfig.java
@@ -0,0 +1,24 @@
+package com.spring_security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Add our user for in memory authentication
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication()
+		.withUser(users.username("parth").password("Test@123").roles("EMPLOYEE"))
+		.withUser(users.username("omkar").password("Test@123").roles("MANAGER"))
+		.withUser(users.username("nikhil").password("Test@123").roles("ADMIN"));
+	}
+	
+}
